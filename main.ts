07477c205c7a39b5bf2e73999d219c391a553e61
rebarbora-mckvak-makecode <rebarbora.mckvak+makecode@gmail.com>
2024-05-10 02:12:43 +0200

    block localization CS

--- main.ts
+++ main.ts
@@ -9,7 +9,8 @@ namespace tm1638 {
     let DIGITS: number[];
 
     //% blockId=TM1638_init
-    //% block="LED&KEY panel pins STB $stb|CLK $clk|DIO $dio"
+    //% block="LED&KEY pin connections STB $stb|CLK $clk|DIO $dio"
+    //% block.loc.cs="LED&KEY připojení pinů STB $stb|CLK $clk|DIO $dio"
     export function init(stb: DigitalPin = DigitalPin.P0, clk: DigitalPin = DigitalPin.P1, dio: DigitalPin = DigitalPin.P2): void {
         DIGITS = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
         _stb = stb;
@@ -65,6 +66,7 @@ namespace tm1638 {
 
     //% blockId=TM1638_clear
     //% block="Clear display and LEDs"
+    //% block.loc.cs="Zhasni displej a LEDky"
     export function clear(): void {
         _command(0x40);
         _stbL();
@@ -77,6 +79,7 @@ namespace tm1638 {
 
     //% blockId=TM1638_set_brightness
     //% block="Set display brightness to $b"
+    //% block.loc.cs="Nastav jas displeje na $b"
     //% b.min=0 b.max=7
     export function setBrightness(b: number = 4): void {
         _brightness = b;
@@ -85,6 +88,7 @@ namespace tm1638 {
 
     //% blockId=TM1638_set_led
     //% block="Turn LED $led|on/off $b"
+    //% block.loc.cs="Rozsviť LED $led|$b"
     //% led.min=0 led.max=7
     export function setLED(led: number, b: boolean): void {
         _command(0x44);
@@ -95,7 +99,8 @@ namespace tm1638 {
     }
 
     //% blockId=TM1638_set_segment
-    //% block="Set segment $segment|to value $val"
+    //% block="Set segment $segment|to $val"
+    //% block.loc.cs="Nastav segment $segment|na $val"
     //% segment.min=0 segment.max=7
     //% val.min=0 val.max=255
     export function setSegment(segment: number, val: number): void {
@@ -108,6 +113,7 @@ namespace tm1638 {
 
     //% blockId=TM1638_show_number
     //% block="Show a number $n on display"
+    //% block.loc.cs="Zobraz číslo $n na displeji"
     export function showNumber(n: number): void {
         let x = n;
         for (let i = 7; i >= 0; i--) {
@@ -118,13 +124,15 @@ namespace tm1638 {
     }
 
     //% blockId=TM1638_show_number_left
-    //% block="Show a number $n on left display"
+    //% block="Show a number $n on the left display"
+    //% block.loc.cs="Zobraz číslo $n na levém displeji"
     export function showNumberLeft(n: number): void {
         _showNumber(0, n);
     }
 
     //% blockId=TM1638_show_number_right
-    //% block="Show a number $n on right display"
+    //% block="Show a number $n on the right display"
+    //% block.loc.cs="Zobraz číslo $n na pravém displeji"
     export function showNumberRight(n: number) : void {
         _showNumber(4, n);
     }
