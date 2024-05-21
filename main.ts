c08eaac2bb5f50d0b9a857f272fbee0dc9736d50
rebarbora-mckvak-makecode <rebarbora.mckvak+makecode@gmail.com>
2024-05-21 02:33:02 +0200

    LED/button/segment indexes changed to 1-8. Support for reading button.

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 //% block="LED&KEY"
 namespace tm1638 {
 
+    const BUTTONS_ID = 15854635;
     let _stb = DigitalPin.P0;
     let _clk = DigitalPin.P1;
     let _dio = DigitalPin.P2;
@@ -16,7 +17,7 @@ namespace tm1638 {
         _stb = stb;
         _clk = clk;
         _dio = dio;
-        pins.setPull(_dio, PinPullMode.PullUp);
+        // pins.setPull(_dio, PinPullMode.PullUp);
         pins.digitalWritePin(_clk, 1);
         pins.digitalWritePin(_dio, 0);
         pins.digitalWritePin(_stb, 1);
@@ -56,6 +57,17 @@ namespace tm1638 {
         }
     }
 
+    function _read_byte() : number {
+        pins.setPull(_dio, PinPullMode.PullUp);
+        let n = 0;
+        for (let i = 0; i < 8; i++) {
+            _clkL();            
+            n |= pins.digitalReadPin(_dio) << i;
+            _clkH();
+        }
+        return n;
+    }
+
     function _write_dsp_ctrl() {
         _command(0x80 | _ON | (_brightness & 7));
     }
@@ -87,26 +99,42 @@ namespace tm1638 {
     }
 
     //% blockId=TM1638_set_led
-    //% block="Turn LED $led|on/off $b"
-    //% block.loc.cs="Rozsviť LED $led|$b"
-    //% led.min=0 led.max=7
-    export function setLED(led: number, b: boolean): void {
+    //% block="Set LED $index (1-8)|on/off $b"
+    //% block.loc.cs="Nastav LED $index (1-8)|$b"
+    //% index.min=1 index.max=8
+    export function setLED(index: number, b: boolean): void {
         _command(0x44);
         _stbL();
-        _set_address((led << 1) + 1);
+        _set_address((index << 1) - 1);
         _write_byte(b ? 1 : 0);
         _stbH();
     }
 
+    //% blockId=TM1638_set_led_on
+    //% block="Turn LED $index (1-8) ON"
+    //% block.loc.cs="Rozsviť LED $index (1-8)"
+    //% index.min=1 index.max=8
+    export function setLED_on(index: number): void {
+        setLED(index, true);
+    }
+
+    //% blockId=TM1638_set_led_off
+    //% block="Turn LED $index (1-8) OFF"
+    //% block.loc.cs="Zhasni LED $index (1-8)"
+    //% index.min=1 index.max=8
+    export function setLED_off(index: number): void {
+        setLED(index, false);
+    }
+
     //% blockId=TM1638_set_segment
-    //% block="Set segment $segment|to $val"
-    //% block.loc.cs="Nastav segment $segment|na $val"
-    //% segment.min=0 segment.max=7
+    //% block="Set segment $index (1-8)|to $val"
+    //% block.loc.cs="Nastav segment $index (1-8)|na $val"
+    //% index.min=1 index.max=8
     //% val.min=0 val.max=255
-    export function setSegment(segment: number, val: number): void {
+    export function setSegment(index: number, val: number): void {
         _command(0x44);
         _stbL();
-        _set_address(segment << 1);
+        _set_address(index - 1 << 1);
         _write_byte(val & 0xFF);
         _stbH();
     }
@@ -116,9 +144,9 @@ namespace tm1638 {
     //% block.loc.cs="Zobraz číslo $n na displeji"
     export function showNumber(n: number): void {
         let x = n;
-        for (let i = 7; i >= 0; i--) {
+        for (let i = 8; i >= 1; i--) {
             const j = x % 10;
-            setSegment(i, x > 0 || i == 7 ? DIGITS[j] : 0);
+            setSegment(i, x > 0 || i == 8 ? DIGITS[j] : 0);
             x = Math.idiv(x, 10);
         }
     }
@@ -127,14 +155,14 @@ namespace tm1638 {
     //% block="Show a number $n on the left display"
     //% block.loc.cs="Zobraz číslo $n na levém displeji"
     export function showNumberLeft(n: number): void {
-        _showNumber(0, n);
+        _showNumber(1, n);
     }
 
     //% blockId=TM1638_show_number_right
     //% block="Show a number $n on the right display"
     //% block.loc.cs="Zobraz číslo $n na pravém displeji"
     export function showNumberRight(n: number) : void {
-        _showNumber(4, n);
+        _showNumber(5, n);
     }
 
     function _showNumber(offset: number, n: number) {
@@ -144,6 +172,43 @@ namespace tm1638 {
             setSegment(offset + i, x > 0 || i == 3 ? DIGITS[j] : 0);
             x = Math.idiv(x, 10);
         }
+    }
+
+    export function keys() : number {
+        _stbL();
+        _write_byte(0x42);
+        let keys = 0;
+        for (let i = 0; i < 4; i++) {
+            let b = _read_byte();
+            keys |= b << i;
+        }
+        _stbH();
+        return keys;
+    }
+
+    //% blockId=TM1638_on_pressed_event
+    //% draggableParameters="reporter"
+    //% block="on button $index pressed"
+    //% block.loc.cs="při stisknutí tlačítka $index (číslo 1-8)"
+    export function onButtonPressEvent(body: (index: number) => void): void {
+        control.onEvent(BUTTONS_ID, DAL.MICROBIT_EVT_ANY, () => {
+            body(control.eventValue());
+        });
+        control.inBackground(() => {
+            let lastKeys = keys();
+            while (true) {
+                const k = keys();
+                if (k != lastKeys) {
+                    let press = (lastKeys ^ k) & k;
+                    for (let i = 0; i < 8; i++) {
+                        if (press & 1 << i)
+                            control.raiseEvent(BUTTONS_ID, i + 1);
+                    }
+                    lastKeys = k;
+                }
+                basic.pause(25); // uses fiber_sleep()
+            }
+        })
     }
 }
 
--- test.ts
+++ test.ts
@@ -15,10 +15,39 @@ for (let i = 0; i < 8; i++) {
 }
 tm._stbH();
 */
-tm1638.setLED(0, true);
-tm1638.setLED(1, true);
-tm1638.setLED(2, false);
-tm1638.setLED(3, true);
-tm1638.setLED(4, true);
+// tm1638.setLED(0, true);
+// tm1638.setLED(1, true);
+// tm1638.setLED(2, false);
+// tm1638.setLED(3, true);
+// tm1638.setLED(4, true);
 tm1638.showNumberLeft(12);
-tm1638.showNumberRight(53);
+tm1638.showNumberRight(50);
+let x = 0;
+let y = 0;
+tm1638.onButtonPressEvent((index) => {
+    // led.toggle(x++, y);
+    // if (x == 5) {
+    //     x = 0;
+    //     y++;
+    //     if (y == 5)
+    //         y = 0;
+    // }
+    // basic.showNumber(index);
+    // tm1638.showNumberRight(index);
+    if (rozsvicene_ledky[index] == 0) {
+        rozsvicene_ledky[index] = 1
+        tm1638.setLED(index, true)
+    } else {
+        rozsvicene_ledky[index] = 0
+        tm1638.setLED(index, false)
+    }
+});
+let rozsvicene_ledky: number[] = []
+for (let i = 1; i <= 8; i++)
+    rozsvicene_ledky[i] = 0;
+
+// while (true) {
+//     let k = tm1638.keys();
+//     tm1638.showNumberRight(k);
+//     basic.pause(10)
+// }
